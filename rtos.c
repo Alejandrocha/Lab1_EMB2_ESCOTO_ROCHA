01060f4fc7513d276446fbccd4f14633a54cf091
Alejandrocha <alvarocha94@gmail.com>
2020-02-10 15:24:03 -0600

    FIRST COMMIT. Add TODOs in rtos.h

---
+++ rtos.c
@@ -0,0 +1,285 @@
+/**
+ * @file rtos.c
+ * @author ITESO
+ * @date Feb 2018
+ * @brief Implementation of rtos API
+ *
+ * This is the implementation of the rtos module for the
+ * embedded systems II course at ITESO
+ */
+
+#include "rtos.h"
+#include "rtos_config.h"
+#include "clock_config.h"
+
+#ifdef RTOS_ENABLE_IS_ALIVE
+#include "fsl_gpio.h"
+#include "fsl_port.h"
+#endif
+/**********************************************************************************/
+// Module defines
+/**********************************************************************************/
+
+#define FORCE_INLINE 	__attribute__((always_inline)) inline
+
+#define STACK_FRAME_SIZE			8
+#define STACK_LR_OFFSET				2
+#define STACK_PSR_OFFSET			1
+#define STACK_PSR_DEFAULT			0x01000000
+
+/**********************************************************************************/
+// IS ALIVE definitions
+/**********************************************************************************/
+
+#ifdef RTOS_ENABLE_IS_ALIVE
+#define CAT_STRING(x,y)  		x##y
+#define alive_GPIO(x)			CAT_STRING(GPIO,x)
+#define alive_PORT(x)			CAT_STRING(PORT,x)
+#define alive_CLOCK(x)			CAT_STRING(kCLOCK_Port,x)
+static void init_is_alive(void);
+static void refresh_is_alive(void);
+#endif
+
+/**********************************************************************************/
+// Type definitions
+/**********************************************************************************/
+
+typedef enum
+{
+	S_READY = 0, S_RUNNING, S_WAITING, S_SUSPENDED
+} task_state_e;
+typedef enum
+{
+	kFromISR = 0, kFromNormalExec
+} task_switch_type_e;
+
+typedef struct
+{
+	uint8_t priority;
+	task_state_e state;
+	uint32_t *sp;
+	void (*task_body)();
+	rtos_tick_t local_tick;
+	uint32_t reserved[10];
+	uint32_t stack[RTOS_STACK_SIZE];
+} rtos_tcb_t;
+
+/**********************************************************************************/
+// Global (static) task list
+/**********************************************************************************/
+
+struct
+{
+	uint8_t nTasks;
+	rtos_task_handle_t current_task;
+	rtos_task_handle_t next_task;
+	rtos_tcb_t tasks[RTOS_MAX_NUMBER_OF_TASKS + 1];
+	rtos_tick_t global_tick;
+} task_list =
+{ 0 };
+
+/**********************************************************************************/
+// Local methods prototypes
+/**********************************************************************************/
+
+static void reload_systick(void);
+static void dispatcher(task_switch_type_e type);
+static void activate_waiting_tasks();
+FORCE_INLINE static void context_switch(task_switch_type_e type);
+static void idle_task(void);
+
+/**********************************************************************************/
+// API implementation
+/**********************************************************************************/
+
+void rtos_start_scheduler(void)
+{
+#ifdef RTOS_ENABLE_IS_ALIVE
+	init_is_alive();
+#endif
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk
+	        | SysTick_CTRL_ENABLE_Msk;
+	reload_systick();
+	for (;;)
+		;
+}
+
+rtos_task_handle_t rtos_create_task(void (*task_body)(), uint8_t priority,
+		rtos_autostart_e autostart)
+{
+/*!
+ * IF TASK_SPACE NOT EMPTY
+ * 	IF AUTOSTART
+ * 		TASK READY
+ *	---
+ * .SUSPEND TASK
+ * .MOVE SP TO TASK STACK INIT
+ * .BASE CONTEXT PARAMETERS
+ * .LOCTICK = 0
+ */
+}
+
+rtos_tick_t rtos_get_clock(void)
+{
+	/*!
+	 * GET SYS_CLK VALUE
+	 */
+	return 0;
+}
+
+void rtos_delay(rtos_tick_t ticks)
+{
+	/*!
+	 * SET CALLING TASK TO SLEEP ticks TICKS
+	 * ASSIGN LOCAL TICKS
+	 * CALL DISPATCHER
+	 */
+
+}
+
+void rtos_suspend_task(void)
+{
+/*!
+ * SUSPEND CALLING TASK
+ * CALL DISPATCHER
+ */
+}
+
+void rtos_activate_task(rtos_task_handle_t task)
+{
+/*!
+ * SET CALLING TASK TO ACTIVE
+ * CALL DISPATCHER
+ */
+}
+
+/**********************************************************************************/
+// Local methods implementation
+/**********************************************************************************/
+
+static void reload_systick(void)
+{
+	SysTick->LOAD = USEC_TO_COUNT(RTOS_TIC_PERIOD_IN_US,
+	        CLOCK_GetCoreSysClkFreq());
+	SysTick->VAL = 0;
+}
+
+static void dispatcher(task_switch_type_e type)
+{
+/*!
+ * SET NEXT TASK TO IDLE
+ * SET HIGH PRI
+ *
+ * FOR EACH TASK
+ * 	IF HIGHEST PRI AND (ACTIVE OR RUNNING)
+ * 		HIGHEST PRI = TASK PRI
+ * 		NEXT TASK = TASK
+ * 	END
+ * 	IF NEXT TASK != CURRENT TASK
+ * 		CHANGE CONTEXT
+ * 	END
+ *
+ */
+}
+
+FORCE_INLINE static void context_switch(task_switch_type_e type)
+{
+/*!
+ * IF NOT FIRST TASK
+ *		SAVE CURRENT SP IN CURRENT TASK STACK
+ * END
+ * CHANGE CURRENT TASK FOR NEXT TASK
+ * SET NEXT TASK TO RUNNING
+ * INVOKE PendSV
+ */
+
+}
+
+static void activate_waiting_tasks()
+{
+/*!
+ * FOR ALL TASKS
+ * 	IF WAITING TASK
+ * 		LOCAL TICK -1
+ * 		IF LOCAL TICK = 0
+ * 			READY TASK
+ * 		END
+ * 	END
+ * END
+ */
+
+}
+
+/**********************************************************************************/
+// IDLE TASK
+/**********************************************************************************/
+
+static void idle_task(void)
+{
+	for (;;)
+	{
+
+	}
+}
+
+/****************************************************/
+// ISR implementation
+/****************************************************/
+
+void SysTick_Handler(void)
+{
+#ifdef RTOS_ENABLE_IS_ALIVE
+	refresh_is_alive();
+#endif
+	activate_waiting_tasks();
+	reload_systick();
+}
+
+void PendSV_Handler(void)
+{
+/*!
+ *  LOAD CPU SP WITH CURRENT TASK SP
+ */
+}
+
+/**********************************************************************************/
+// IS ALIVE SIGNAL IMPLEMENTATION
+/**********************************************************************************/
+
+#ifdef RTOS_ENABLE_IS_ALIVE
+static void init_is_alive(void)
+{
+	gpio_pin_config_t gpio_config =
+	{ kGPIO_DigitalOutput, 1, };
+
+	port_pin_config_t port_config =
+	{ kPORT_PullDisable, kPORT_FastSlewRate, kPORT_PassiveFilterDisable,
+	        kPORT_OpenDrainDisable, kPORT_LowDriveStrength, kPORT_MuxAsGpio,
+	        kPORT_UnlockRegister, };
+	CLOCK_EnableClock(alive_CLOCK(RTOS_IS_ALIVE_PORT));
+	PORT_SetPinConfig(alive_PORT(RTOS_IS_ALIVE_PORT), RTOS_IS_ALIVE_PIN,
+	        &port_config);
+	GPIO_PinInit(alive_GPIO(RTOS_IS_ALIVE_PORT), RTOS_IS_ALIVE_PIN,
+	        &gpio_config);
+}
+
+static void refresh_is_alive(void)
+{
+	static uint8_t state = 0;
+	static uint32_t count = 0;
+	SysTick->LOAD = USEC_TO_COUNT(RTOS_TIC_PERIOD_IN_US,
+	        CLOCK_GetCoreSysClkFreq());
+	SysTick->VAL = 0;
+	if (RTOS_IS_ALIVE_PERIOD_IN_US / RTOS_TIC_PERIOD_IN_US - 1 == count)
+	{
+		GPIO_PinWrite(alive_GPIO(RTOS_IS_ALIVE_PORT), RTOS_IS_ALIVE_PIN,
+		        state);
+		state = state == 0 ? 1 : 0;
+		count = 0;
+	} else //
+	{
+		count++;
+	}
+}
+#endif
+///
